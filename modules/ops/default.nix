f0786efe2349830022ebd4e185665cddb72b9e58
Dominik Klementowski <dominik232@gmail.com>
2023-01-18 20:38:20 +0100

    python for scripts

--- modules/ops/default.nix
+++ modules/ops/default.nix
@@ -2,6 +2,12 @@
 
 let
   vars = import ../../vars.nix;
+  pythonPackages = p: with p; [
+    boto3
+    pyyaml
+    validate-email
+    docopt
+  ];
 in {
   users.users.${vars.userName}.packages = with pkgs; [
     vagrant
@@ -12,6 +18,10 @@ in {
     podman-compose
     pods
     k9s
+    awscli2
+
+    # Python for runing helper scripts like AWS SSO
+    (pkgs.python3.withPackages pythonPackages)
   ];
 
   virtualisation = {
