a242013e1820ec6bc55312d02200c0653a22a582
Dominik Klementowski <dominik@dklmnt.com>
2023-01-28 13:52:18 +0100

    Merge parts for shell

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,10 +1,55 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ./zsh.nix
-    ./tmux.nix
-    ./neovim.nix
-    ./pass.nix
-  ];
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "candy";
+    };
+  };
+
+  programs.tmux = {
+    enable = true;
+    historyLimit = 10000;
+    terminal = "screen-256color";
+    keyMode = "vi";
+    extraConfig = builtins.readFile ./config/tmux.conf;
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true; # This fails on zsh for some reason
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    plugins = [ pkgs.vimPlugins.packer-nvim ];
+    extraConfig = ''
+      luafile ${ ./config/neovim.lua }
+    '';
+    extraPackages = with pkgs; [
+      nodejs-18_x
+      python310
+      rustc
+      cargo
+      gcc
+    ];
+  };
+
+  programs.password-store = {
+    enable = true;
+    package = pkgs.pass-wayland.withExtensions (es: with es; [
+      pass-update
+      pass-otp
+    ]);
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
 }
